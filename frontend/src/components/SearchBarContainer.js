97d99c795dceda51e1e780d9479b6460a606cd29
Igor Veyner <igorveyner95@gmail.com>
2021-09-03 10:16:39 -0400

    integrate api request

--- frontend/src/components/SearchBarContainer.js
+++ frontend/src/components/SearchBarContainer.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import { getCities } from '../services/api'
 
 const SearchBarContainer = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  const [offset, setOffset] = useState(0)
   
   const handleChange = (e) => {
     setSearchTerm(e.target.value)
@@ -12,7 +14,7 @@ const SearchBarContainer = () => {
       <input type="text" value={searchTerm} onChange={handleChange} />
 
       {/* temp button */}
-      <button>Search</button>
+      <button onClick={() => getCities(searchTerm, offset)}>Search</button>
     </div>
   )
 }
