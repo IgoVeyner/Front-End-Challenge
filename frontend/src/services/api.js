15db3804c95b8f90c33712706fe22a5fd85313b5
Igor Veyner <igorveyner95@gmail.com>
2021-09-03 10:13:43 -0400

    add api file with fetch request to cities with query

---
+++ frontend/src/services/api.js
@@ -0,0 +1,21 @@
+const url = "http://localhost:3030/"
+const citiesUrl = url + 'cities'
+
+export const getCities = (searchTerm, offset = 0) => {
+  const queryPrefix = '?query',
+    filterPrefix = '&filter=',
+    limit = '&limit=10',
+    offsetPrefix = '&offset='
+
+  const CreateCitiesUrlWithQuery = () => {
+    const convertedOffset = (offset * 10)
+
+    return citiesUrl + queryPrefix + 
+      filterPrefix + searchTerm + 
+      limit + offsetPrefix + convertedOffset
+  }
+
+  fetch(CreateCitiesUrlWithQuery())
+  .then(data => data.json())
+  .then(parsed => console.log(parsed))
+}
