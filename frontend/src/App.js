ce56541ba9fcfd00fa8d2496a14207b9ee8aff9e
Igor Veyner <igorveyner95@gmail.com>
2021-09-03 17:25:38 -0400

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header'
 import SearchBarContainer from './components/SearchBarContainer';
 import List from './components/List';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SearchBarContainer />
       <List />
     </div>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <p>Select your favorite cities</p>
+    </div>
+  )
+}
+
+export default Header
