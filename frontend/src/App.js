4ddf8c19098dda5f63d011f4d8b02a07aa2123f0
Igor Veyner <igorveyner95@gmail.com>
2021-09-03 17:15:15 -0400

    remove old code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,6 @@ import SearchBarContainer from './components/SearchBarContainer';
 import List from './components/List';
 
 function App() {
-  // const [searchResults, setSearchResults] = useState({})
-
-  // const updateSearchResults = (returnedResults) => {
-  //   setSearchResults(returnedResults)
-  // }
-
   return (
     <div className="App">
       <SearchBarContainer />
