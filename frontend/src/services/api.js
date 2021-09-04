e87b41ec27254d9736ebadbaa582a6e069c3e2c8
Igor Veyner <igorveyner95@gmail.com>
2021-09-04 17:16:48 -0400

    add update preferences request

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -29,3 +29,21 @@ export const getPreferences = (offset = 0) => {
   return fetch(createFullPreferencesUrl())
     .then(parseData)
 }
+
+export const updatePreferences = (preferences, task) => {
+  const taskBool = task === "ADD" ? true : false 
+  const newPreferences = {}
+
+  preferences.forEach(key => {
+    newPreferences[key] = taskBool
+  })
+
+  return fetch(preferencesUrl, {
+    method: "PATCH",
+    headers: {
+      'Content-Type': 'application/json', 
+      'Accepts': 'application/json'
+    },
+    body: JSON.stringify(newPreferences)
+  })
+} 
