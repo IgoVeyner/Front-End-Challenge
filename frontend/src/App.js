c2cf651ff9cab61eedb027dd8aa2f8d42bce11c7
Igor Veyner <igorveyner95@gmail.com>
2021-09-05 15:58:28 -0400

    add fetch for all favorites on initial mount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './sass/app.sass';
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Header from './components/Header'
 import SearchBar from './components/SearchBar';
 import CitiesListContainer from './components/CitiesListContainer';
 import FavoritesListContainer from './components/FavoritesListContainer';
+import { getFullPreferences } from './services/api';
+import useFetchRequest from './hooks/useFetchRequest';
 
 function App() {
   const [favoritesListId, setFavoritesListId] = useState(1)
@@ -11,6 +13,23 @@ function App() {
   const resetFavoritesList = () => {
     setFavoritesListId(favoritesListId + 1)
   }
+
+  const getAllPreferences = useCallback(
+    () => {
+      getFullPreferences()
+      .then(resp => {
+        // TODO: error handling for 500 error codes
+        console.log(resp)
+      })
+      .catch(error => {
+        // TODO: error handling for non 500 codes
+        console.log(error)
+      })
+    },
+    [],
+  )
+
+  useFetchRequest(getAllPreferences)
 
   return (
     <div className="App">
