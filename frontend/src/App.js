f4c3ee68b8dac1382ee0523e17d0a3ca6e786880
Igor Veyner <igorveyner95@gmail.com>
2021-09-05 16:14:16 -0400

    render rest of components after getting full preferences

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './sass/app.sass';
 import { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from './components/Header'
 import SearchBar from './components/SearchBar';
 import CitiesListContainer from './components/CitiesListContainer';
@@ -7,9 +8,14 @@ import FavoritesListContainer from './components/FavoritesListContainer';
 import { getFullPreferences } from './services/api';
 import useFetchRequest from './hooks/useFetchRequest';
 import AppLoading from './components/AppLoading';
+import { setPreferences } from './redux/actions/preferencesActions';
 
 function App() {
   const [favoritesListId, setFavoritesListId] = useState(1)
+  const [busy, setBusy] = useState(true)
+  const [error, setError] = useState(false)
+
+  const dispatch = useDispatch()
 
   const resetFavoritesList = () => {
     setFavoritesListId(favoritesListId + 1)
@@ -17,34 +23,49 @@ function App() {
 
   const getAllPreferences = useCallback(
     () => {
+      const setFullPreferences = (data) => dispatch(setPreferences(data))
+
       getFullPreferences()
       .then(resp => {
         // TODO: error handling for 500 error codes
+        if (resp.statusCode === 500) {
+          // handle error here
+          setError(true)
+        } else {
+          setBusy(false)
+          setFullPreferences(resp)
+        }
         console.log(resp)
       })
       .catch(error => {
         // TODO: error handling for non 500 codes
         console.log(error)
+        setError(true)
       })
     },
     [],
   )
 
   const renderComponents = () => {
-    return <AppLoading />
+    if (error) return <div>Please try again!</div>
+    if (busy) return <AppLoading />
+    return (
+      <>
+        <Header />
+        <SearchBar />
+        <CitiesListContainer />
+        <FavoritesListContainer 
+          key={favoritesListId} 
+          onPress={resetFavoritesList}
+        /> 
+      </>
+    )
   }
 
   useFetchRequest(getAllPreferences)
 
   return (
     <div className="App">
-      {/* <Header />
-      <SearchBar />
-      <CitiesListContainer />
-      <FavoritesListContainer 
-        key={favoritesListId} 
-        onPress={resetFavoritesList}
-      /> */}
       {renderComponents()}
     </div>
   );
