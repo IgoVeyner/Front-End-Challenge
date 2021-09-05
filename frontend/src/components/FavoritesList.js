01394c8b824c5c384acc9f7711389e545aa8a1b7
Igor Veyner <igorveyner95@gmail.com>
2021-09-05 10:28:09 -0400

    create stub of favorites list

--- frontend/src/components/FavoritesList.js
+++ frontend/src/components/FavoritesList.js
@@ -3,9 +3,41 @@ import { useSelector } from 'react-redux'
 const FavoritesList = () => {
   const favorites = useSelector(state => state.preferences)
   
+  const prevPage = () => {
+    // TODO
+  }
+
+  const nextPage = () => {
+    // TODO
+  }
+
+  const prevDisableCheck = () => {
+    // TODO
+  }
+
+  const nextDisableCheck = () => {
+    // TODO
+  }
+
   return (
-    <div>
-    
+    <div className="list-container">
+      <div>
+        <button 
+          onClick={prevPage} 
+          disabled={prevDisableCheck()} >
+          Prev
+        </button>
+
+        <button 
+          onClick={nextPage} 
+          disabled={nextDisableCheck()} >
+          Next
+        </button>
+      </div>
+
+      <div className="list-inner-container">
+  
+      </div>
     </div>
   )
 }
