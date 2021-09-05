70ac223a7c3501d76ca665fdb1b8ce4a2c74da10
Igor Veyner <igorveyner95@gmail.com>
2021-09-05 13:41:35 -0400

    add functionality to get city data on mount

--- frontend/src/components/FavoritesListItem.js
+++ frontend/src/components/FavoritesListItem.js
@@ -1,6 +1,30 @@
+import { useState, useCallback } from "react"
+import useFetchRequest from "../hooks/useFetchRequest"
+import { getCity } from "../services/api"
+
 const FavoritesListItem = ({ id }) => {
+  const [cityData, setCityData] = useState({})
+
+  const getCityData = useCallback(
+    () => {
+      getCity(id)
+      .then(data => {
+        // TODO: error handling here
+        console.log(data)
+      })
+      .catch(error => {
+        // TODO: error handling here even though 500 error code wont be caught here
+        console.log(error)
+      })
+    }, [id]
+  )
+
+  useFetchRequest(getCityData)
+
   return (
-    <div>{id}</div>
+    <div>
+       {id}
+    </div>
   )
 }
 
