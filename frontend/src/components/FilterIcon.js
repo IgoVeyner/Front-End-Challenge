8182307017417eca48469c6d5f88c28d4d37ebbe
Igor Veyner <igorveyner95@gmail.com>
2021-09-04 21:42:05 -0400

    add filter icon styles

--- frontend/src/components/FilterIcon.js
+++ frontend/src/components/FilterIcon.js
@@ -1,6 +1,6 @@
 const FilterIcon = () => {
   return (
-    <div className="">
+    <div className="filter-container">
       <svg 
         xmlns="http://www.w3.org/2000/svg" 
         version="1.1" 
