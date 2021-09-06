83e170f277df9d3f6dd18338e7ce98151c7c3b92
Igor Veyner <igorveyner95@gmail.com>
2021-09-06 11:07:02 -0400

    reset offset on favorites click

--- frontend/src/components/FavoritesListItem.js
+++ frontend/src/components/FavoritesListItem.js
@@ -1,15 +1,22 @@
 import { useDispatch } from 'react-redux'
+import { resetOffset } from '../redux/actions/offsetActions'
 import { updateSearchTerm } from '../redux/actions/searchTermActions'
 
 const FavoritesListItem = ({ city }) => {
   const dispatch = useDispatch()
 
   const lookupCity = () => dispatch(updateSearchTerm(city.name))
+  const resetSearchOffset = () => dispatch(resetOffset())
   
+  const handleClick = () => {
+    lookupCity()
+    resetSearchOffset()
+  }
+
   return (
     <div
       className="favorites-list-item" 
-      onClick={lookupCity}
+      onClick={handleClick}
     >
       <div>{city.name} ({city.subcountry})</div>
     </div>
