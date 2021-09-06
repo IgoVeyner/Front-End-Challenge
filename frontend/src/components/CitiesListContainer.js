66ffa84f76a69a77101cf664c5710b51577af63d
Igor Veyner <igorveyner95@gmail.com>
2021-09-06 11:02:15 -0400

    update pagination button clicks to wait for more clicks before execution

--- frontend/src/components/CitiesListContainer.js
+++ frontend/src/components/CitiesListContainer.js
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { updateOffset } from "../redux/actions/offsetActions"
 import CitiesList from "./CitiesList"
@@ -6,22 +7,54 @@ import Pagination from "./Pagination"
 import SearchError from "./SearchError"
 
 const CitiesListContainer = () => {
+  const interval = useRef(null)
+  const nextPageClicks = useRef(0)
+  const prevPageClicks = useRef(0)
+
   const cities = useSelector(state => state.cities)
   const offset = useSelector(state => state.offset)
   const loading = useSelector(state => state.searchLoading)
   const searchError = useSelector(state => state.searchError)
 
   const dispatch = useDispatch()
-
-  // TODO: debounce theses requests or else pressing buttons a lot of times becomes 
-  //       a very bad experiences
-  const nextPage = () => dispatch(updateOffset(10))
-  const prevPage = () => dispatch(updateOffset(-10))
-
+  
   const prevDisableCheck = () => offset === 0 ? true : false 
   const nextDisableCheck = () => offset + 10 >= cities.total ? true : false
 
   const disabledStatus = [prevDisableCheck(), nextDisableCheck()]
+
+
+  const onNextClick = () => {
+    const nextPage = () => {
+      const calulateMaxNumber = () => {
+        return cities.total - cities.total % 10
+      }
+
+      const newOffset = Math.min(calulateMaxNumber() - offset, nextPageClicks.current * 10)
+      dispatch(updateOffset(newOffset))
+      nextPageClicks.current = 0
+    }
+
+    clearInterval(interval.current)
+    nextPageClicks.current += 1
+    updateInterval(nextPage, 500)
+  }
+
+  const onPrevClick = () => {
+    const prevPage = () => {
+      const newOffset = Math.max(0 - offset, prevPageClicks.current * -10)
+      dispatch(updateOffset(newOffset))
+      prevPageClicks.current = 0
+    }
+
+    clearInterval(interval.current)
+    prevPageClicks.current += 1
+    updateInterval(prevPage, 500)
+  }
+
+  const updateInterval = (callback, time) => {
+    interval.current = setInterval(callback, time)
+  }
 
   const renderInnerComponent = () => {
     if (loading) return <Loading />
@@ -32,8 +65,8 @@ const CitiesListContainer = () => {
   return (
     <div className="list-container"> 
       <Pagination 
-        onNextClick={nextPage}
-        onPrevClick={prevPage}
+        onNextClick={onNextClick || null}
+        onPrevClick={onPrevClick}
         disabledStatus={disabledStatus}
         results={cities}
         startValue={offset}
