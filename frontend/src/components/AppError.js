394bf9923c9bf2032f6977896fa3abd4c94bb964
Igor Veyner <igorveyner95@gmail.com>
2021-09-06 19:36:17 -0400

    update app error styles

--- frontend/src/components/AppError.js
+++ frontend/src/components/AppError.js
@@ -2,7 +2,7 @@ const AppError = ({ onPress }) => {
   return (
     <div className="app-error">
       <h1>Oops!</h1>
-      <h2>Something went wrong...</h2>
+      <h2>Failed to load User Preferences...</h2>
       <button onClick={onPress}>Try Again</button>
     </div>
   )
