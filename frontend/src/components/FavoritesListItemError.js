c0d6f7336188d2bb71cd12e42d4f03fa9929373c
Igor Veyner <igorveyner95@gmail.com>
2021-09-06 21:48:59 -0400

    update error styles

--- frontend/src/components/FavoritesListItemError.js
+++ frontend/src/components/FavoritesListItemError.js
@@ -1,7 +1,7 @@
 const FavoritesListItemError = ({ onPress }) => {
   return (
     <div className="favorites-item-error" onClick={onPress}>
-      <div>Failed to get city data...</div>
+      <div>Failed to get city data</div>
       <div>Click to Try Again</div>
     </div>
   )
