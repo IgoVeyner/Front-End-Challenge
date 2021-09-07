afef8a9c49ffcdfaf15b1c77001743c99faa254f
Igor Veyner <igorveyner95@gmail.com>
2021-09-07 16:38:16 -0400

    pass in dispatch callback

--- frontend/src/hooks/useNextClick.js
+++ frontend/src/hooks/useNextClick.js
@@ -1,17 +1,12 @@
 import { useCallback } from "react"
-import { useDispatch } from "react-redux"
-import { updatePreferncesOffset } from '../redux/actions/preferencesOffsetActions'
 import { onNextClick } from "../services/pagination"
 
-const useNextClick = (list, offset, counter, interval) => {
-  const dispatch = useDispatch()
-
+const useNextClick = (list, offset, counter, interval, dispatchCallback) => {
   const nextPage = useCallback(
     () => {
-      const updateOffset = (data) => dispatch(updatePreferncesOffset(data))
-      onNextClick(list.total, offset, counter, interval, updateOffset)
+      onNextClick(list.total, offset, counter, interval, dispatchCallback)
     },
-    [list, offset, dispatch, interval, counter],
+    [list, offset, interval, counter, dispatchCallback],
   )
   
   return nextPage
--- frontend/src/hooks/usePrevClick.js
+++ frontend/src/hooks/usePrevClick.js
@@ -1,15 +1,10 @@
 import { useCallback } from "react"
-import { useDispatch } from "react-redux"
-import { updatePreferncesOffset } from '../redux/actions/preferencesOffsetActions'
 import { onPrevClick } from "../services/pagination"
 
-const usePrevClick = (offset, counter, interval) => {
-  const dispatch = useDispatch()
-
+const usePrevClick = (offset, counter, interval, dispatchCallback) => {
   const prevPage = useCallback(() => {
-    const updateOffset = (data) => dispatch(updatePreferncesOffset(data))
-    onPrevClick(offset, counter, interval, updateOffset)
-  },[counter, offset, interval, dispatch])
+    onPrevClick(offset, counter, interval, dispatchCallback)
+  },[counter, offset, interval, dispatchCallback])
 
   return prevPage
 }
