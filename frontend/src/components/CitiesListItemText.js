df3f5262bc1aa01e25117db9dab7f9aa7d8f9da6
Igor Veyner <igorveyner95@gmail.com>
2021-09-07 09:32:59 -0400

    bolden matched words in search results

--- frontend/src/components/CitiesListItemText.js
+++ frontend/src/components/CitiesListItemText.js
@@ -1,10 +1,38 @@
+import { useSelector } from "react-redux"
+
 const CitiesListItemText = ({ data }) => {
+  const {name, subcountry, country} = data
+  const searchTerm = useSelector(state => state.searchTerm)
+
+  const checkForMatch = (word) => {
+    if (searchTerm.trim() === "") return word
+    const position = word.toLowerCase().search(searchTerm.toLowerCase())
+    return position !== -1 ? boldenMatch(word, position) : word
+  } 
+
+  const boldenMatch = (word, startPosition) => {
+    const endPosition = startPosition + searchTerm.length
+
+    const preBold = word.slice(0, startPosition)
+    const bolden = word.slice(startPosition, endPosition)
+    const postBold = word.slice(endPosition, word.length)
+    
+    return [preBold, bolden, postBold].map((word, index) => {
+      return spanifyAndBolden(word, index === 1)
+    })
+  }
+
+  const spanifyAndBolden = (word, bold) => {
+    if (bold) return <span className='bold'>{word}</span>
+    return <span>{word}</span>
+  }
+  
   return (
     <div className="city-text-container">
-      <h1 className="city-name">{data.name}</h1>
+      <h1 className="city-name">{checkForMatch(name)}</h1>
 
       <div className="city-text">
-        {data.subcountry} - {data.country}
+        {checkForMatch(subcountry)} - {checkForMatch(country)}
       </div>
     </div>
   )
