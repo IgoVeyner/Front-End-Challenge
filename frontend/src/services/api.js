f91422a7ac5832de0abd08d4da3733ebf4890eea
Igor Veyner <igorveyner95@gmail.com>
2021-09-07 19:04:34 -0400

    remove useCancelableFetch, found working solution

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -54,21 +54,3 @@ export const updatePreferences = (id, task) => {
     body: JSON.stringify(newPreference)
   })
 } 
-
-export const makeCancelable = (promise) => {
-  let hasCanceled_ = false
-
-  const wrappedPromise = new Promise((resolve, reject) => {
-    promise.then(
-      val => hasCanceled_ ? reject({isCanceled: true}) : resolve(val),
-      error => hasCanceled_ ? reject({isCanceled: true}) : reject(error)
-    )
-  })
-
-  return {
-    promise: wrappedPromise,
-    cancel() {
-      hasCanceled_ = true;
-    },
-  }
-}
