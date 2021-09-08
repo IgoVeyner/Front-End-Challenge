dbc3adb693dca888ba10959851a9393d9afb9575
Igor Veyner <igorveyner95@gmail.com>
2021-09-08 11:32:39 -0400

    abstract dispatch call and pass in as argument

--- frontend/src/hooks/usePagination.js
+++ frontend/src/hooks/usePagination.js
@@ -1,25 +1,20 @@
 import { useRef } from "react"
-import { useDispatch } from "react-redux"
-import { updateOffset } from "../redux/actions/offsetActions"
 import { prevDisableCheck, nextDisableCheck } from '../services/pagination'
 import useNextClick from "../hooks/useNextClick"
 import usePrevClick from "../hooks/usePrevClick"
 
-const usePagination = (list, offset, busy, error) => {
+const usePagination = (list, offset, busy, error, dispatchCallback) => {
   const interval = useRef(null)
   const nextPageClicks = useRef(0)
   const prevPageClicks = useRef(0)
-
-  const dispatch = useDispatch()
-  const updateCitiesOffset = (data) => dispatch(updateOffset(data))
   
   const disabledStatus = [
     prevDisableCheck(busy, error, list, offset), 
     nextDisableCheck(busy, error, list, offset)
   ]
 
-  const nextPage = useNextClick(list, offset, nextPageClicks, interval, updateCitiesOffset)
-  const prevPage = usePrevClick(offset, prevPageClicks, interval, updateCitiesOffset)
+  const nextPage = useNextClick(list, offset, nextPageClicks, interval, dispatchCallback)
+  const prevPage = usePrevClick(offset, prevPageClicks, interval, dispatchCallback)
 
   return {
     disabledStatus,
