eb0662927abff3d5574cfb443fddc33eea3f7125
Igor Veyner <igorveyner95@gmail.com>
2021-09-07 20:51:01 -0400

    finalize app error page

--- frontend/src/components/AppError.js
+++ frontend/src/components/AppError.js
@@ -1,8 +1,7 @@
 const AppError = ({ onPress }) => {
   return (
     <div className="app-error">
-      <h1>Oops!</h1>
-      <h2>Failed to load User Preferences...</h2>
+      <h1>Failed to load User Preferences</h1>
       <button onClick={onPress}>Try Again</button>
     </div>
   )
